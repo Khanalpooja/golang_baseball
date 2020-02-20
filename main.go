0f4b13155e0f4fdcad9151cad8de1b27685af049
poojakhanal <khanalpooja425@gmail.com>
2020-02-20 11:14:20 -0600

    Revised go

--- main.go
+++ main.go
@@ -1,22 +1,59 @@
+// Prepared by: Pooja Khanal, A25288740
+// Prepared for : Assignment 1, CS524, Spring 2020
+// Submitted on : 02/20/2020
+
+//Program Description:
+// Main tasks covered by this program are
+// 1. This program calculates the player statistics, batting avaerage, slugging and base percentage.
+// 2. Checks if inconsisent data are present in any player line.
+// 3. Calculates overall batting average for players without any error in their input data
+
 package main
 
-// add preamble in the code
-// add your name, A#, seciton, details to follow in the progran
 import (
 	"bufio"
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/fatih/color"
 )
 
 // Entry point of the program // main function
 func main() {
-	var fileName string
-	fmt.Println("Enter filename")
+	color.Cyan(`Welcome to the player statistics calculator test program. I am going to
+read players from an input data file.  You will tell me the name of
+your input file.  I will store all of the players in a list,
+compute each player's averages and then write the resulting team report 
+to your output file.`)
+
+	var fileName, outFile, option string
+	fmt.Println("Enter the name of your input file")
 	fmt.Scan(&fileName)
 	players, errors := getPlayers(fileName)
 	printPlayerReport(players)
 	printErrorReport(errors)
+	fmt.Println("Fo you want to write your output to a file? Type Y/N")
+	fmt.Scan(&option)
+	if option == "Y" || option == "y" {
+		fmt.Println("Give a name for your output file")
+		fmt.Scan(&outFile)
+		// outputf, _ := os.Create(outFile)
+		// fmt.Println()
+		// writer := bufio.NewWriter(outputf)
+		// defer writer.Flush()
+
+		fmt.Println("Output written to", outFile)
+		fmt.Println("End of program, GoodBye!")
+	} else if option == "N" || option == "n" {
+		fmt.Println("End of Program, GoodBye!")
+	} else {
+		fmt.Println("End of Program, GoodBye!")
+	}
+
+	// printPlayerReport(players)
+	// printErrorReport(errors)
+
 }
 
 func check(e error) {
